62704393fc3c075508e35effae80433ace0fb03c
Tomiwa-Akinyemi <akintomiwa15@gmail.com>
2023-05-02 13:20:02 +0100

    Set default timeout for mongo connection

--- dags/bots/FetchDataFromMongo.py
+++ dags/bots/FetchDataFromMongo.py
@@ -1,7 +1,7 @@
 import pymongo
 import pandas
 
-connection_url=pymongo.MongoClient(f"mongodb+srv://dap:dap@cluster0.dcyzoyt.mongodb.net/test")
+connection_url=pymongo.MongoClient(f"mongodb+srv://dap:dap@cluster0.dcyzoyt.mongodb.net/test", maxPoolSize=50, maxIdleTimeMS=10000, waitQueueTimeoutMS=10000, connectTimeoutMS=20000)
 db_name=connection_url.CrimeDataset
 collection_name = "ChicagoCrime"
 # connection_url=pymongo.MongoClient(f"mongodb://dap:dap@localhost:27017")
@@ -18,7 +18,7 @@ try:
     results = collection.find({},{ "_id": 0,"ï»¿ID":0})#.limit(20)
     print("Done running the query.")
     df = pandas.DataFrame(list(results))
-    #print(df.head())
+    # print(df.head())
 except pymongo.errors.OperationFailure as exc:  # If the collection doesn't exist
     print("This collection doesn't exist")
     print(exc)
